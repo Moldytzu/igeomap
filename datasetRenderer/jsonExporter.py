d47dfae47905a37e2d6f3109e5be4255eededd99
Moldytzu <m.alex123@yahoo.com>
2023-10-07 20:26:39 +0300

    genereaza json pentru toate granulele

--- datasetRenderer/jsonExporter.py
+++ datasetRenderer/jsonExporter.py
@@ -4,7 +4,7 @@ def genereazaLinkImagineNASA(imagine):
     return f"https://e4ftl01.cr.usgs.gov//WORKING/BRWS/Browse.001/2014.11.20/{imagine}.SRTMGL1.2.jpg"
 
 def genereazaJSON(fisier, index):
-    output = "{\n"
+    output = "{"
 
     with HgtParser(fisier) as parser:
         # luam coordonatele geografice
@@ -30,34 +30,43 @@ def genereazaJSON(fisier, index):
         print(f"{coordMin=} {coordMax=} {altMed=}")
 
         # coordonatele patrulaterului
-        output += f"coord:[\n"
-        output += f"    [{long}, {lat}],\n"
-        output += f"    [{long + 1}, {lat + 1}],\n"
-        output += f"],\n"
+        output += f"coord:["
+        output += f"    [{long}, {lat}],"
+        output += f"    [{long + 1}, {lat + 1}],"
+        output += f"],"
 
         # text placeholder
-        output += f"text: \"\",\n"
+        output += f"text: \"\","
 
         # altitudinea maxima si minima
-        output += f"coordaltmax: [{coordMax[1], coordMax[0]}],\n"
-        output += f"coordaltmin: [{coordMin[1], coordMin[0]}],\n"
-        output += f"altmax: {coordMax[2]},\n"
-        output += f"altmin: {coordMin[2]},\n"
-        output += f"altmed: {altMed},\n"
+        output += f"coordaltmax: [{coordMax[1], coordMax[0]}],"
+        output += f"coordaltmin: [{coordMin[1], coordMin[0]}],"
+        output += f"altmax: {coordMax[2]},"
+        output += f"altmin: {coordMin[2]},"
+        output += f"altmed: {altMed},"
 
         # imaginile
-        output += f"image: \"{genereazaLinkImagineNASA(fisier[10:17])}\",\n"
-        output += f"imagehr: \"{genereazaLinkImagineNASA(fisier[10:17])}\",\n" # imagine de calitate mare
+        output += f"image: \"{genereazaLinkImagineNASA(fisier[10:17])}\","
+        output += f"imagehr: \"{genereazaLinkImagineNASA(fisier[10:17])}\"," # imagine de calitate mare
 
         # sol
-        output += f"soilComp: [],\n"
-        output += f"soilRes: [],\n"
+        output += f"soilComp: [],"
+        output += f"soilRes: [],"
 
         # indice
-        output += f"granuleNumber: {index},\n"
+        output += f"granuleNumber: {index},"
 
     output += "}"
 
     return output
 
-print(genereazaJSON("./dataset/N47E023.hgt", 0))
+fisier = open("out.json","w+")
+
+fisier.write("[")
+i = 1
+for n in range(44, 49, 1):
+    for e in range(20, 30, 1):
+        fisier.write(genereazaJSON(f"./dataset/N{n}E0{e}.hgt", i))
+        fisier.write(",")
+        i += 1
+fisier.write("]")

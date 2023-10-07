b59fae22bfd7e936af8fdc3fe57ee81aedb97c47
Moldyztu <m.alex123@yahoo.com>
2023-10-07 13:14:14 +0300

    curatare si comentare

--- datasetParser/parser.py
+++ datasetParser/parser.py
@@ -1,24 +1,28 @@
 from gmalthgtparser import HgtParser
 
+# globale
 rezolutie = 0.001 # in productie seteaza la 0.001 (latura de 1000)
 latura = int(1/rezolutie)
 lista = []
 
-print(f"{rezolutie=} {latura=}")
+print(f"parser: {rezolutie=} {latura=}")
 
-# de reamintit ca latitudinea e X si longitudinea e Y molozule
+# de reamintit ca latitudinea e X si longitudinea e Y
 
 # wrapper pentru a prelua elevatia dintr un punct geografic
-def getElevation(parser, long, lat):
+def iaElevatie(parser, long, lat):
     return parser.get_elevation((long, lat))[2]
 
-fisier = "./dataset/N44E022.hgt"
+fisier = "./dataset/N47E024.hgt"
 with HgtParser(fisier) as parser:
+    # luam coordonatele geografice
     coord = fisier[10:][:7]
     lat = int(coord[4:])
     long = int(coord[1:][:2])
+
+    # le inseram in lista de puncte
     for y in range(1, latura, 1):
         for x in range(1, latura, 1):
-            lista.append((x  * rezolutie, y  * rezolutie, getElevation(parser, long + y * rezolutie, lat + x * rezolutie)))
+            lista.append((x  * rezolutie, y  * rezolutie, iaElevatie(parser, long + y * rezolutie, lat + x * rezolutie)))
 
-print("gata")
+print("parser: gata")
--- datasetParser/plot.py
+++ datasetParser/plot.py
@@ -1,19 +1,21 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import itertools
 from matplotlib  import cm
-from mpl_toolkits.mplot3d import Axes3D
-
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
 
 from parser import * # bibliotea noastra magica
  
 # Using zip function and Unpacking the values
 x, y, z = zip(*lista)
 
+# scoatem marginile albe
 plt.axis("off")
 plt.margins(x=0, y=0, tight=True)
+
+# punem punctele pe imagine
 plt.scatter(x, y, s=15,c=z, marker = ',', cmap = cm.gist_gray)
+
+print("plot: gata scatter")
+
+# salvam figura
 plt.savefig("test.png", dpi=1000, bbox_inches='tight', pad_inches = 0)
-plt.show()
+
+print("plot: gata save")

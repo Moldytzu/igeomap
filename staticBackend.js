e08e52dbcdb575842bd268d449c57c58dedcffaa
Moldyztu <m.alex123@yahoo.com>
2023-10-08 11:21:24 +0300

    schimba portul backendului si foloseste imagini hostate local

--- staticBackend.js
+++ staticBackend.js
@@ -1,6 +1,4 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
 app.use(express.static("datasetRenderer/render/"));
-app.listen(port, console.log(`Server ruleaza pe port ${port}`));
+app.listen(3001, console.log(`Server ruleaza pe port ${3001}`));
